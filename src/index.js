d4573562166669abd817c45f47ba12dfb45db881
Ricardo Pires Dall Oglio <ricardo.tech@live.com>
2019-06-14 22:05:10 -0300

    put  in the right place

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
+const app = express()
+
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-
-const app = express()
 
 mongoose.connect(
   'mongodb+srv://omnistack:omnistack@cluster0-2sryy.mongodb.net/test?retryWrites=true&w=majority',
@@ -29,4 +29,4 @@ app.use(
 
 app.use(require('./routes'))
 
-server.listen(3000)
+server.listen(3333)
